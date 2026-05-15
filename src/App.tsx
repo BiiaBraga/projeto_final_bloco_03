6d4bd5da0c256be9c051cc5aa0ef1dd91640b7c4
BiiaBraga <beatrizsagrado9@gmail.com>
2026-05-15 13:07:13 -0300

    feat: CRUD de categoria

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
 import Home from './pages/home/Home'
+import ListaCategorias from './components/categorias/listacategorias/ListaCategorias'
+import FormCategoria from './components/categorias/formcategorias/FormCategoria'
+import DeletarCategoria from './components/categorias/deletarcategoria/DeletarCategoria'
 
 function App() {
     return (
@@ -13,8 +16,10 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/home" element={<Home />} />
-                    <Route path="/categorias" element={<h1>Categorias</h1>} />
-                    <Route path="/cadastrarCategoria" element={<h1>Cadastrar Categoria</h1>} />
+                    <Route path="/categorias" element={<ListaCategorias />} />
+                    <Route path="/cadastrarCategoria" element={<FormCategoria />} />
+                    <Route path="/editarCategoria/:id" element={<FormCategoria />} />
+                    <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
                     <Route path="/produtos" element={<h1>Produtos</h1>} />
                     <Route path="/cadastrarProduto" element={<h1>Cadastrar Produto</h1>} />
                 </Routes>
